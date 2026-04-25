d5469258a66a2d43b93b91408705b1247d142853
수연 <suyeon@suyeon-ui-MacBookAir.local>
2026-04-25 19:14:21 +0900

    Connect Apps Script backend endpoint

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script 웹앱 URL을 배포 후 여기에 붙여넣으세요
-window.SURVEY_ENDPOINT = 'YOUR_APPS_SCRIPT_URL_HERE';
+window.SURVEY_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxOFf6-SjMoAdjXaaB_GrkDWQhQNQzkdLiXHQu33Z2v5zZslkUe4_HN8qfSpIIwKIHJNA/exec';
