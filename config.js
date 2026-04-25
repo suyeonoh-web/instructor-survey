2ccd6953f11bf8c1a4be409246228d2e71c148ad
수연 <suyeon@suyeon-ui-MacBookAir.local>
2026-04-25 20:13:20 +0900

    Update Apps Script endpoint URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script 웹앱 URL을 배포 후 여기에 붙여넣으세요
-window.SURVEY_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxOFf6-SjMoAdjXaaB_GrkDWQhQNQzkdLiXHQu33Z2v5zZslkUe4_HN8qfSpIIwKIHJNA/exec';
+window.SURVEY_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxtVxoDVW0V-Krs4KHhU7C9uQp1ldczK0mwaC1Rx-hRAIHQoG0Tc_W3HbdLgOk2UXvH/exec';
